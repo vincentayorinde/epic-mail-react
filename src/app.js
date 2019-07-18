e377aba1d540a265bb2f903659ef41542aa320d5
vincentayorinde <vincentayorinde@yahoo.com>
2019-07-18 20:26:57 +0100

    - install react     - install react-dom [Finishes #167382013]

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-class App {}
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Welcome to Epic Mail App</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
